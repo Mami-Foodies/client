bb1d22235a9bf6614353322f2c735311f5e4bdab
Danang Putra Bahari <danangbahari89@gmail.com>
2019-11-29 13:58:57 +0700

    fix master

--- main.js
+++ main.js
@@ -188,7 +188,7 @@ $(document).ready(function(){
       $('#searchRes').val('')
       response.forEach(restaurant => {
         $('#content-resto').append(`
-        <div class="card mr-2 mt-3 card-content" style="width: 18rem;" onclick="detailRestaurant(${restaurant.id})">
+        <div class="card mr-2 mt-3 card-content" style="width: 18rem;" onclick="getRestaurantDetail(${restaurant.id})">
           <img src="${restaurant.thumb}" class="card-img-top" alt="gambar">
           <div class="card-body d-flex flex-column justify-content-between">
               <div class="">
@@ -214,26 +214,80 @@ $(document).ready(function(){
     })
   }
 
-  function detailRestaurant(id){
-
+  function getRestaurantDetail(id){
     $.ajax({
-      url: `http://localhost:3000/api/zomato/restaurant/${id}`,
-      method: 'GET'
+      method:'get',
+      url:`http://localhost:3000/api/zomato/restaurant/${id}`
     })
-    .done(response => {
-      const responseFiltered = {
-        name: response.name,
-        locality: response.locality,
-        address: response.location.address,
-        longitude: response.longtitude,
-        latitude: response.latitude
-
-      }
-      console.log(response)  
-    })
-    .fail(err => {
+    .done(restaurant=>{
       
+      const restObj = {
+        name: restaurant.name,
+        latitude: restaurant.location.latitude,
+        longitude: restaurant.location.longitude
+      }
+      
+      getPlaceId(restObj)
+  
+  
+  
+      const individualMenu={
+        price:10000,
+        convertFrom:'IDR',
+        convertInto:'USD'
+      }
+  
+      getConvertedRate(individualMenu)
+  
+  
     })
+    .fail(err=>{
+      console.log(err)
+    })
+  
+  }
+  
+  function getPlaceId(objRestLocation)
+  {
+    $.ajax({
+      method: 'get',
+      url: `http://localhost:3000/api/gmap/getplaceidbylonglat/?restname=${objRestLocation.name}&latitude=${objRestLocation.latitude}&longitude=${objRestLocation.longitude}`
+    })
+    .done(placeId=>{
+      $('#restaurant-location').empty()
+      $('#restaurant-location').append(`
+        woiwoiwowiowio
+        ${placeId}
+  
+        <div class="mt-5">
+          <!-- Highlight a place or an address -->
+          <iframe width="100%" height="450" frameborder="0" style="border:0"
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:${placeId}&key=AIzaSyB7M7GX2lx2Jd72pCYcHb0mpZed0XoQEcU" allowfullscreen></iframe>
+  
+      `)
+  
+    })
+    .fail(err=>{
+      console.log(err)
+    })
+  }
+  
+  function getConvertedRate(individualMenu)
+  {
+    $.ajax({
+      method:'get',
+      url:`http://localhost:3000/api/currency/getcurrencyrate/?price=${individualMenu.price}&convertFrom=${individualMenu.convertFrom}&convertInto=${individualMenu.convertInto}`
+    })
+    .done(result=>{
+      $('#converted-rate').empty()
+      $('#converted-rate').append(`
+        ${individualMenu.convertInto} ${result}
+      `)
+    })
+    .fail(err=>{
+      console.log(err)
+    })
+    
   }
 
 
@@ -247,109 +301,3 @@ $(document).ready(function(){
 
 
 
-  // gawean JAP
-  $(document).ready(function(){
-    getRestaurantDetail()
-    
-    
-  })
-
-
-function getRestaurantDetail()
-{
-  // maquis belezza
-  const dummyId = 7402064
-  $.ajax({
-    method:'get',
-    url:`http://localhost:3000/api/zomato/restaurant/${dummyId}`
-  })
-  .done(restaurant=>{
-    $('#restaurant-info').empty()
-    $('#restaurant-info').append(`
-    <div class="">
-      <h5 class="card-title">${restaurant.name}</h5>
-      <p class="text-muted">${restaurant.location.address}</p>
-      <p class="card-text">Phone</p>
-      <p class="text-muted">${restaurant.phone_numbers}</p>
-      <p class="card-text">Cuisine</p>
-      <p class="text-muted">${restaurant.cuisines}</p>
-      <p class="card-text">Opening Hours</p>
-      <p class="card-text"></p>
-    </div>
-    `)
-
-
-    const restObj = {
-      name: restaurant.name,
-      latitude: restaurant.location.latitude,
-      longitude: restaurant.location.longitude
-    }
-    console.log("TCL: restObj", restObj)
-    
-    getPlaceId(restObj)
-
-
-
-    const individualMenu={
-      price:10000,
-      convertFrom:'IDR',
-      convertInto:'USD'
-    }
-
-    getConvertedRate(individualMenu)
-
-
-  })
-  .fail(err=>{
-    console.log(err)
-  })
-
-}
-
-
-
-function getPlaceId(objRestLocation)
-{
-  $.ajax({
-    method: 'get',
-    url: `http://localhost:3000/api/gmap/getplaceidbylonglat/?restname=${objRestLocation.name}&latitude=${objRestLocation.latitude}&longitude=${objRestLocation.longitude}`
-  })
-  .done(placeId=>{
-    $('#restaurant-location').empty()
-    $('#restaurant-location').append(`
-      woiwoiwowiowio
-      ${placeId}
-
-      <div class="mt-5">
-        <!-- Highlight a place or an address -->
-        <iframe width="100%" height="450" frameborder="0" style="border:0"
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:${placeId}&key=AIzaSyB7M7GX2lx2Jd72pCYcHb0mpZed0XoQEcU" allowfullscreen></iframe>
-
-    `)
-
-  })
-  .fail(err=>{
-    console.log(err)
-  })
-}
-
-
-
-
-function getConvertedRate(individualMenu)
-{
-  $.ajax({
-    method:'get',
-    url:`http://localhost:3000/api/currency/getcurrencyrate/?price=${individualMenu.price}&convertFrom=${individualMenu.convertFrom}&convertInto=${individualMenu.convertInto}`
-  })
-  .done(result=>{
-    $('#converted-rate').empty()
-    $('#converted-rate').append(`
-      ${individualMenu.convertInto} ${result}
-    `)
-  })
-  .fail(err=>{
-    console.log(err)
-  })
-  
-}
